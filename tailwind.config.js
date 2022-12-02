1269020ef52c0c56997da2b43b1d1f54bf0eaba5
dev-acc-practice <development-practice@proton.me>
2022-12-03 00:07:21 +0200

    styling header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
+    extend: {
+      backgroundImage: {
+        'hero': 'url(/images/mobile/image-hero.jpg)',
+      }
+    },
   },
   plugins: [],
 }
